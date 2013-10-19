8287513c359b9e7ea9932f322c8889c640a75982
Nicole Sullivan <nicole@stubbornella.org>
2013-10-19 10:48:13 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MessageBoard::Application.config.secret_key_base = '208f232b38a9b4d5ef7fe966aa229dbaeb898807655371f5fc04d8d3c510514bcdf22f59675d9560536d7f180e17d3d31b55ec4389ab10fb558c57a5f0878d9b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MessageBoard::Application.config.session_store :cookie_store, key: '_message_board_session'
